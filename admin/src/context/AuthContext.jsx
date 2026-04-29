10f2447213e5619b90398bdf6ad18597c27ff531
khushi chandravanshi <khushichandravanshi377@gmail.com>
2026-04-30 03:40:06 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://bestmart-backend.onrender.com"
+    let serverUrl = "https://bestmart-backendfinal.onrender.com"
 
     let value = {
       serverUrl
